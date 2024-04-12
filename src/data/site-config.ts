bea3f02cabc60e967543d7d9a4f017ee49eaf3d0
Maggie Martin <maggierozay@gmail.com>
2024-04-12 15:45:29 +0200

    Built out the rest of the pages

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -56,11 +56,11 @@ const siteConfig: SiteConfig = {
         },
         {
             text: 'Adopt-A-Horse',
-            href: '/blog'
+            href: '/adopt-a-horse'
         },
         {
             text: 'Contact',
-            href: '/tags'
+            href: '/contact'
         }
     ],
     footerNavLinks: [
@@ -74,20 +74,20 @@ const siteConfig: SiteConfig = {
         },
         {
             text: 'Adopt-A-Horse',
-            href: '/blog'
+            href: '/adopt-a-horse'
         },
         {
             text: 'Contact',
-            href: '/tags'
+            href: '/contact'
         }
     ],
     socialLinks: [
         {
             text: 'Facebook',
-            href: 'https://dribbble.com/'
+            href: 'https://www.facebook.com/people/Carpe-Diem-Farms/100064746390013/?ref=hl'
         },
     ],
-    hero: {
+    /*hero: {
         title: 'Hi There & Welcome to My Corner of the Web!',
         text: "I'm **Ethan Donovan**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence. My approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products. I have a profound appreciation for top-notch software, visual design, and the principles of product-led growth. Feel free to explore some of my coding endeavors on <a href='https://github.com/JustGoodUI/dante-astro-theme'>GitHub</a> or follow me on <a href='https://twitter.com/justgoodui'>Twitter/X</a>.",
         image: {
@@ -107,7 +107,7 @@ const siteConfig: SiteConfig = {
         formUrl: '#'
     },
     postsPerPage: 8,
-    projectsPerPage: 8
+    projectsPerPage: 8*/
 };
 
 export default siteConfig;
